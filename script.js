765bbf5cc4de1775951e421e2369aadf6a0bce70
Siddharth Sisodia <139268988+siddharth2504@users.noreply.github.com>
2025-05-06 00:57:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+let arr = [1,2,3,4]
+let div = document.querySelector("#output")
+let prom = new Promise((res, rej) => {
+	setTimeout(() => {
+		let brr = arr.filter(item => item % 2 === 0)
+		div.innerText = brr.join(" ")
+	    res(brr)
+	}, 1000)
+})
+prom
+	.then(brr => {
+	    setTimeout(() => {
+	    	let crr = brr.map(item => item*2)
+			div.innerText = crr.join(" ")
+	    }, 3000)
+     })
+
+    
